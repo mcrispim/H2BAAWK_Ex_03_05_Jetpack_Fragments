4ba5de2b7a5089a7d14ce2a59ca0de6e8fcb3e2e
Mauro Crispim <mcrispim@hotmail.com>
2022-03-13 17:35:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ex_03_05_jetpackfragments/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ex_03_05_jetpackfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+const val STAR_SIGN_ID = "STAR_SIGN_ID"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
